e95c29e4d6150452f25814aec0c875319993450b
Qingwei Lan <qingweilan@gmail.com>
2022-02-10 14:21:10 -0600

    [move] fix printing

--- src/move.pl
+++ src/move.pl
@@ -15,26 +15,28 @@ can_move(X, Y) :-
 
 % move the hero to position (X, Y)
 goto(X, Y) :-
-    % checks whether position is within bounds of (X, Y)
-    in_bounds(X, Y),
-    % check position (X, Y) is valid for a move
-    can_move(X, Y),
-    % get hero's current position
-    heropos(CX, CY),
-    % remove hero from current location
-    retract( heropos(CX, CY) ),
-    % move hero to new location
-    assert( heropos(X, Y) ),
-    writef('Hero moved from (%d, %d) to (%d, %d)', [CX, CY, X, Y]).
-
-goto(X, Y) :- writef('FAILED to move to (%d, %d)', [X, Y]).
-
+    (
+        % checks whether position is within bounds of (X, Y)
+        in_bounds(X, Y),
+        % check position (X, Y) is valid for a move
+        can_move(X, Y)
+    ->
+        % get hero's current position
+        heropos(CX, CY),
+        % remove hero from current location
+        retract( heropos(CX, CY) ),
+        % move hero to new location
+        assert( heropos(X, Y) ),
+        writef('\t[ok]\n\n')
+    ;
+        write('\t[FAILED]\n\n')
+    ).
 
 % move right by 1 step
-rr() :- heropos(X, Y), NX #= X+1, goto(NX, Y).
+rr() :- heropos(X, Y), NX #= X+1, write('Moving right... '), goto(NX, Y).
 % move left by 1 step
-ll() :- heropos(X, Y), NX #= X-1, goto(NX, Y).
+ll() :- heropos(X, Y), NX #= X-1, write('Moving left... '), goto(NX, Y).
 % move up by 1 step
-uu() :- heropos(X, Y), NY #= Y+1, goto(X, NY).
+uu() :- heropos(X, Y), NY #= Y+1, write('Moving up... '), goto(X, NY).
 % move down by 1 step
-dd() :- heropos(X, Y), NY #= Y-1, goto(X, NY).
+dd() :- heropos(X, Y), NY #= Y-1, write('Moving down... '), goto(X, NY).
