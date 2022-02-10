93c683fc28bf937a08cd66955ebeb8f8df4f40b5
Qingwei Lan <qingweilan@gmail.com>
2022-02-10 11:49:02 -0600

    [move] simplify naming

--- src/move.pl
+++ src/move.pl
@@ -9,10 +9,10 @@ move(CX, CY, X, Y) :-
     writef('Moved from (%d, %d) to (%d, %d)', [CX, CY, X, Y]).
 
 % move right by 1 step
-move_r() :- heropos(X, Y), NX #= X+1, move(X, Y, NX, Y).
+rr() :- heropos(X, Y), NX #= X+1, move(X, Y, NX, Y).
 % move left by 1 step
-move_l() :- heropos(X, Y), NX #= X-1, move(X, Y, NX, Y).
+ll() :- heropos(X, Y), NX #= X-1, move(X, Y, NX, Y).
 % move up by 1 step
-move_u() :- heropos(X, Y), NY #= Y+1, move(X, Y, X, NY).
+uu() :- heropos(X, Y), NY #= Y+1, move(X, Y, X, NY).
 % move down by 1 step
-move_d() :- heropos(X, Y), NY #= Y-1, move(X, Y, X, NY).
+dd() :- heropos(X, Y), NY #= Y-1, move(X, Y, X, NY).
