169521ed903abb1bf1a411ba25a3fe7f9de174ca
LinkMa <754490227@qq.com>
2022-02-21 17:13:04 -0600

    [Add peril] Update peril file to simplify

---
+++ src/peril.pl
@@ -0,0 +1,20 @@
+:- use_module(library(clpfd)).
+:- dynamic peril/3.
+
+fight_peril(X, Y) :-
+    (
+    peril(X, Y, M)
+    ->
+    % get current health
+    hero_health(H),
+    write(' Found a peril! Fighting!\n After fighting with peril, current status is: \n'),
+    % impact health by margin
+    NH #= H + M,
+    % update health with NH
+    retract( hero_health(H) ),
+    assert( hero_health(NH) ),
+    % print current health
+    print_hero_status()
+    ;
+    true
+    ).
