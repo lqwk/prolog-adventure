7d89e16a1b3bcc6070d46cac61b7fb0cf739d4f5
Qingwei Lan <qingweilan@gmail.com>
2022-02-22 11:20:28 -0600

    [peril] indent

--- src/peril.pl
+++ src/peril.pl
@@ -3,19 +3,19 @@
 
 fight_peril(X, Y) :-
     (
-    peril(X, Y, M)
+        peril(X, Y, M)
     ->
-    % get current health
-    hero_health(H),
-    write(' Found a peril! Fighting!\n After fighting with peril, current status is: \n'),
-    % impact health by margin
-    NH #= H + M,
-    % update health with NH
-    retract( hero_health(H) ),
-    assert( hero_health(NH) ),
-    % print current health
-    print_hero_status()
+        % get current health
+        hero_health(H),
+        write(' Found a peril! Fighting!\n After fighting with peril, current status is: \n'),
+        % impact health by margin
+        NH #= H + M,
+        % update health with NH
+        retract( hero_health(H) ),
+        assert( hero_health(NH) ),
+        % print current health
+        print_hero_status()
     ;
-    true
+        true
     ).
 
