b63e7fad01d514d6eeae7a14ba612f53fe8bfe7e
Qingwei Lan <qingweilan@gmail.com>
2022-02-22 15:28:30 -0600

    [map] initialization message

---
+++ src/init.pl
@@ -0,0 +1,7 @@
+% initialization file
+
+:- initialization
+    write('\n\n===================================================\n'),
+    write('\tWELCOME TO OPEN-WORLD ADVENTURE!\n'),
+    write('\tTYPE start(). TO START THE GAME!\n'),
+    write('===================================================\n\n\n').
