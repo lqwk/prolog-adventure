a9daa08ae9c764482db6855fca9dec3a52ec0b48
Qingwei Lan <qingweilan2022@u.northwestern.edu>
2022-03-03 17:10:37 -0600

    [discovery] print weather info on each move

--- src/discovery.pl
+++ src/discovery.pl
@@ -1,16 +1,26 @@
 % contains logic related to discovering cells around the hero
 
 % show the information for the cell on which the hero is on
-
 % cannot see cell info in foggy weather
 show_cell_info(C, R) :- weather(C, R, foggy), write('Foggy, cannot see'), !.
 
 % otherwise, print cell information
-show_cell_info(C, R) :- wall(C, R), write('Wall'), fail.
-show_cell_info(C, R) :- rock(C, R), write('Rock'), fail.
-show_cell_info(C, R) :- gem(C, R), write('Gem'), fail.
-show_cell_info(C, R) :- peril(C, R, _), write('Peril'), fail.
-show_cell_info(_, _).
+show_cell_info(C, R) :-
+    show_object_info(C, R),
+    write(' \t '),
+    show_weather_info(C, R).
+
+% show cell object information
+show_object_info(C, R) :- wall(C, R), write('Wall'), fail.
+show_object_info(C, R) :- rock(C, R), write('Rock'), fail.
+show_object_info(C, R) :- gem(C, R), write('Gem'), fail.
+show_object_info(C, R) :- peril(C, R, _), write('Peril'), fail.
+show_object_info(_, _).
+
+% show weather information
+show_weather_info(C, R) :- weather(C, R, rainy), write('Rainy'), fail.
+show_weather_info(C, R) :- weather(C, R, thunder), write('Thunder'), fail.
+show_weather_info(_, _).
 
 % the discovery module prints out information related to the 4
 % adjacent cells to the left, right, up, down of the hero
